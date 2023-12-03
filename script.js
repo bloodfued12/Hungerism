8e1a127b3d7cb36265877b1b7b04e360c1292d3f
Kurresailakshmareddy <Kurresailakshmareddy@gmail.com>
2023-12-03 15:18:51 +0000

    update

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let menuBtn = document.getElementById("menuBtn")
 let sideNav = document.getElementById("sideNav")    
 let menu = document.getElementById("menu") 
 
-sideNav.style.right = "-250px";
+sideNav.style.right = "-350px";
 
 menuBtn.onclick = function(){
-    if(sideNav.style.right == "-250px"){
+    if(sideNav.style.right == "-350px"){
         sideNav.style.right = "0";
         menu.src = "images/close.png";
     }
     else{
-        sideNav.style.right = "-250px";
+        sideNav.style.right = "-350px";
         menu.src = "images/menu.png";
     }
 }
